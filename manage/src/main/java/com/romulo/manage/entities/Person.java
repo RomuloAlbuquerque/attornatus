2b4ab58ac0d0023e56d6d308243e99e99c0bc500
RomuloAlbuquerque <66214784+RomuloAlbuquerque@users.noreply.github.com>
2023-01-12 02:08:54 -0300

    Person resource

---
+++ manage/src/main/java/com/romulo/manage/entities/Person.java
@@ -0,0 +1,64 @@
+package com.romulo.manage.entities;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.Objects;
+
+public class Person implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	private Long id;
+	private String name;
+	private LocalDate birthDate;
+	
+	public Person() {
+		
+	}
+
+	public Person(Long id, String name, LocalDate birthDate) {
+		this.id = id;
+		this.name = name;
+		this.birthDate = birthDate;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public LocalDate getBirthDate() {
+		return birthDate;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Person other = (Person) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+	
+}
