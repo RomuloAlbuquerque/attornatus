6aff536e9f0b165fa970a5cc3f7e7fbf447c26e6
RomuloAlbuquerque <66214784+RomuloAlbuquerque@users.noreply.github.com>
2023-01-13 23:54:51 -0300

    Find Person By Id, exception handling

--- manage/src/main/java/com/romulo/manage/resources/PersonResource.java
+++ manage/src/main/java/com/romulo/manage/resources/PersonResource.java
@@ -4,6 +4,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,6 +22,12 @@ public class PersonResource {
 	public ResponseEntity<List<PersonDTO>> findAll() {
 		List<PersonDTO> list = service.findAll();
 		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<PersonDTO> findById(@PathVariable Long id) {
+		PersonDTO dto = service.findById(id);
+		return ResponseEntity.ok().body(dto);
 	}
 }
 		
--- manage/src/main/java/com/romulo/manage/services/PersonService.java
+++ manage/src/main/java/com/romulo/manage/services/PersonService.java
@@ -1,6 +1,7 @@
 package com.romulo.manage.services;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.romulo.manage.dto.PersonDTO;
 import com.romulo.manage.entities.Person;
 import com.romulo.manage.repositories.PersonRepository;
+import com.romulo.manage.services.exceptions.EntityNotFoundException;
 
 @Service
 public class PersonService {
@@ -21,5 +23,12 @@ public class PersonService {
 	public List<PersonDTO> findAll(){
 		List<Person> list = repository.findAll();
 	return list.stream().map(x -> new PersonDTO(x)).collect(Collectors.toList());
+	}
+
+	@Transactional(readOnly = true)
+	public PersonDTO findById(Long id) {
+		Optional<Person> obj = repository.findById(id);
+		Person entity = obj.orElseThrow(()-> new EntityNotFoundException("Entity Not Found"));
+		return new PersonDTO(entity);
 	}
 }
